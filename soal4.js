e680e0613fe22ba4b46a125a153e2715678d2929
Teuku Ibrahim <68468112+Drackoo@users.noreply.github.com>
2025-04-07 20:18:56 +0700

    Add files via upload

--- soal4.js
+++ soal4.js
@@ -3,4 +3,5 @@ function adaDuplikat(arr) {
     return unik.size !== arr.length;
   }
 
-  
+  console.log(adaDuplikat([20, 1, 3, 2, 4, 6, 8, 5, 7, 9, 11, 13, 15, 10,  12, 14, 16, 18, 20, 17, 19]))
+  console.log(adaDuplikat([1,2,3,4,5]))
